8df8ed955b6c40c51bad9b1d1bc73d6186f20dee
blahkheart <dannithomx@gmail.com>
2023-01-16 13:39:10 +0100

    updated Fallback.sol

--- task-fallback/Fallback.sol
+++ task-fallback/Fallback.sol
@@ -16,42 +16,47 @@ contract UserBalance {
     }
 
     ///////////////////Modifier///////////////////
-    modifier onlyOwner(){
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
-    modifier haveDeposit(){ 
-        if(balances[msg.sender] == 0){
+    modifier haveDeposit() {
+        if (balances[msg.sender] == 0) {
             revert MakeADepositFirst();
         }
         _;
     }
-    modifier isAmountLessThanFee(uint _amount){
-        if(_amount < FEE) {
+    modifier isAmountLessThanFee(uint _amount) {
+        if (_amount < FEE) {
             revert AmountTooSmall();
         }
         _;
     }
 
-    receive() external payable {}
+    ///////////////Fallback////////////////////
+    fallback() external payable {}
+
     /*
-    * @Dev deposit a new student
-    * @Param _amount: deposit amount
-    */
+     * @Dev deposit a new student
+     * @Param _amount: deposit amount
+     */
     function deposit() public payable {
         balances[msg.sender] += msg.value;
         emit FundsDeposited(msg.sender, msg.value);
     }
 
-    function addFund(uint256 _amount )public haveDeposit isAmountLessThanFee(_amount) {
+    function addFund(
+        uint256 _amount
+    ) public haveDeposit isAmountLessThanFee(_amount) {
         balances[msg.sender] += _amount;
     }
+
     function withdraw() public onlyOwner {
-        (bool success,) = payable(msg.sender).call{ value: address(this).balance}("");
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(success);
     }
-
-  
 
     /*
      * @Dev get balance of caller
@@ -67,19 +72,28 @@ contract UserBalance {
     }
 
     mapping(address => UserDetails) userDetails;
+
     /*
      * @Dev set user details
      * @Param name
      * @Param age
      */
     function setUserDetails(string calldata name, uint256 age) public {
-        userDetails[msg.sender] = UserDetails({name:name,age: age});
+        userDetails[msg.sender] = UserDetails({name: name, age: age});
         emit ProfileUpdated(msg.sender);
     }
+
     /*
      * @Dev get user details
      */
-    function getUserDetail() public view returns(UserDetails memory _userDetails) {
+    function getUserDetail()
+        public
+        view
+        returns (UserDetails memory _userDetails)
+    {
         _userDetails = userDetails[msg.sender];
     }
+
+    ///////////////Receive////////////////////
+    receive() external payable{}
 }
